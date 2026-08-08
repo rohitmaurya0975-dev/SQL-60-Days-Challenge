1b365a5f7f7e959b59cbb82f95ee68dc8d8429b0
rohitmaurya0975-dev <rohitmaurya0975@gmail.com>
2026-08-08 10:13:02 +0530

    Create DAY01.sql

---
+++ Day-01/DAY01.sql
@@ -0,0 +1,86 @@
+-- ============================================
+-- SQL 60 Days Challenge
+-- Day 1: SQL Basics
+-- Author: Rohit Maurya
+-- ============================================
+
+
+-- ============================================
+-- TABLE CREATION
+-- ============================================
+
+CREATE TABLE employee2 (
+    employee_id INT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    department VARCHAR(50),
+    salary NUMERIC(10,2),
+    city VARCHAR(50)
+);
+
+
+-- ============================================
+-- INSERT SAMPLE DATA
+-- ============================================
+
+INSERT INTO employee2 (employee_id, name, department, salary, city)
+VALUES
+(101, 'AMIT', 'Sales', 45000, 'Delhi'),
+(102, 'Riya', 'HR', 52000, 'Mumbai'),
+(103, 'Karan', 'IT', 60000, 'Mumbai'),
+(104, 'Neha', 'Sales', 48000, 'Delhi'),
+(105, 'Rahul', 'IT', 70000, 'Bengaluru'),
+(106, 'Priya', 'HR', 55000, 'Mumbai'),
+(107, 'Ankit', 'Sales', 45000, 'Jaipur'),
+(108, 'Meena', 'IT', 70000, 'Pune');
+
+
+-- ============================================
+-- Q1: Display all employee details.
+-- ============================================
+
+SELECT *
+FROM employee2;
+
+
+-- ============================================
+-- Q2: Display only Name and Salary.
+-- ============================================
+
+SELECT name, salary
+FROM employee2;
+
+
+-- ============================================
+-- Q3: Show employees whose salary is greater than 50000.
+-- ============================================
+
+SELECT *
+FROM employee2
+WHERE salary > 50000;
+
+
+-- ============================================
+-- Q4: Show employees from Delhi.
+-- ============================================
+
+SELECT *
+FROM employee2
+WHERE city = 'Delhi';
+
+
+-- ============================================
+-- Q5: Display all employees ordered by Salary
+--     from Highest to Lowest.
+-- ============================================
+
+SELECT *
+FROM employee2
+ORDER BY salary DESC;
+
+
+-- ============================================
+-- Q6: Display unique departments.
+-- ============================================
+
+SELECT DISTINCT department
+FROM employee2;
